316e96714f3fb945d4e495001457c844576a5289
Tiffany Wang <tinnywang@gmail.com>
2014-05-01 13:42:02 -0700

    Sylvester matrix library, white cube rendered

--- js/rubiks.js
+++ js/rubiks.js
@@ -1,4 +1,10 @@
+var canvas;
 var gl;
+var shaderProgram;
+var vertexPosition;
+var perspectiveMatrix;
+var cubeVerticesBuffer;
+var cubeVerticesIndexBuffer;
 
 function initWebGL(canvas) {
 	if (!window.WebGLRenderingContext) {
@@ -46,7 +52,7 @@ function getShader(gl, id) {
 function initShaders() {
 	var fragmentShader = getShader(gl, 'fragmentShader');
 	var vertexShader = getShader(gl, 'vertexShader');
-	var shaderProgram = gl.createProgram();
+	shaderProgram = gl.createProgram();
 	gl.attachShader(shaderProgram, fragmentShader);
 	gl.attachShader(shaderProgram, vertexShader);
 	gl.linkProgram(shaderProgram);
@@ -54,18 +60,116 @@ function initShaders() {
 		console.log("Unable to initialize the shader program");
 	}
 	gl.useProgram(shaderProgram);
-	var vertexPosition = gl.getAttribLocation(shaderProgram, 'vertexPosition');
+	vertexPosition = gl.getAttribLocation(shaderProgram, 'vertexPosition');
 	gl.enableVertexAttribArray(vertexPosition);
 }
 
+function initCubeBuffer() {
+  // Create a buffer for the cube's vertices.
+  
+  cubeVerticesBuffer = gl.createBuffer();
+  
+  // Select the cubeVerticesBuffer as the one to apply vertex
+  // operations to from here out.
+  
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesBuffer);
+  
+  // Now create an array of vertices for the cube.
+  
+  var vertices = [
+    // Front face
+    -1.0, -1.0,  1.0,
+     1.0, -1.0,  1.0,
+     1.0,  1.0,  1.0,
+    -1.0,  1.0,  1.0,
+
+    // Back face
+    -1.0, -1.0, -1.0,
+    -1.0,  1.0, -1.0,
+     1.0,  1.0, -1.0,
+     1.0, -1.0, -1.0,
+    
+    // Top face
+    -1.0,  1.0, -1.0,
+    -1.0,  1.0,  1.0,
+     1.0,  1.0,  1.0,
+     1.0,  1.0, -1.0,
+    
+    // Bottom face
+    -1.0, -1.0, -1.0,
+     1.0, -1.0, -1.0,
+     1.0, -1.0,  1.0,
+    -1.0, -1.0,  1.0,
+    
+    // Right face
+     1.0, -1.0, -1.0,
+     1.0,  1.0, -1.0,
+     1.0,  1.0,  1.0,
+     1.0, -1.0,  1.0,
+    
+    // Left face
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0,  1.0,
+    -1.0,  1.0,  1.0,
+    -1.0,  1.0, -1.0
+  ];
+  
+  // Now pass the list of vertices into WebGL to build the shape. We
+  // do this by creating a Float32Array from the JavaScript array,
+  // then use it to fill the current vertex buffer.
+  
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  
+  // Build the element array buffer; this specifies the indices
+  // into the vertex array for each face's vertices.
+  
+  cubeVerticesIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
+  
+  // This array defines each face as two triangles, using the
+  // indices into the vertex array to specify each triangle's
+  // position.
+  
+  var cubeVertexIndices = [
+    0,  1,  2,      0,  2,  3,    // front
+    4,  5,  6,      4,  6,  7,    // back
+    8,  9,  10,     8,  10, 11,   // top
+    12, 13, 14,     12, 14, 15,   // bottom
+    16, 17, 18,     16, 18, 19,   // right
+    20, 21, 22,     20, 22, 23    // left
+  ]
+  
+  // Now send the element array to GL
+  
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
+      new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
+}
+
+function drawRubiksCube() {
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	perspectiveMatrix = makePerspective(45,
+										canvas.width / canvas.height,
+										0.1,
+										100.0);
+	initCubeBuffer();
+	loadIdentity();
+	mvTranslate([0.0, 0.0, -6.0]);
+	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesBuffer);
+	gl.vertexAttribPointer(vertexPosition, 3, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
+	setMatrixUniforms();
+	gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+}
+
 function start() {
-	var canvas = document.getElementById("glcanvas");
+	canvas = document.getElementById("glcanvas");
 	gl = initWebGL(canvas);
 	initShaders();
 	if (gl) {
-		gl.clearColor(1.0, 1.0, 1.0, 1.0);
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
 		gl.enable(gl.DEPTH_TEST);
 		gl.depthFunc(gl.LEQUAL);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	}
+	drawRubiksCube();
 }
