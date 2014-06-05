4e34ee8691c97d36b6dc5cae8f575d5cb73026bc
Tiffany Wang <tinnywang@gmail.com>
2014-06-05 01:42:44 -0700

    cleaned up code, fixed rotation axis, black outline

--- js/rubiks.js
+++ js/rubiks.js
@@ -9,7 +9,7 @@ var rotationMatrix = mat4.create();
 var cubeVerticesBuffer;
 var cubeVerticesIndexBuffer;
 var cubeVerticesColorBuffer;
-var cubeOutlineIndexBuffer;
+var cubeOutlineBuffer;
 
 var mouseDown = false;
 var x_init;
@@ -78,17 +78,22 @@ function initShaders() {
 }
 
 function initCubeBuffer() {
-    // Create a buffer for the cube's vertices.
+    // cube outline
+    cubeOutlineBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeOutlineBuffer);
+    var cubeOutlineIndices = [
+        0,  1,  2,  3,  0,
+        4,  5,  6,  7,  4,
+        8,  9,  10, 11, 8,
+        12, 13, 14, 15, 12,
+        16, 17, 18, 19, 16,
+        20, 21, 22, 23, 20
+    ]
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeOutlineIndices), gl.STATIC_DRAW);
 
+    // cube vertices
     cubeVerticesBuffer = gl.createBuffer();
-
-    // Select the cubeVerticesBuffer as the one to apply vertex
-    // operations to from here out.
-
     gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesBuffer);
-
-    // Now create an array of vertices for the cube.
-
     var vertices = [
         // Front face
         -1.0, -1.0,  1.0,
@@ -125,24 +130,12 @@ function initCubeBuffer() {
         -1.0, -1.0,  1.0,
         -1.0,  1.0,  1.0,
         -1.0,  1.0, -1.0
-            ];
-
-    // Now pass the list of vertices into WebGL to build the shape. We
-    // do this by creating a Float32Array from the JavaScript array,
-    // then use it to fill the current vertex buffer.
-
+    ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-    // Build the element array buffer; this specifies the indices
-    // into the vertex array for each face's vertices.
-
+    // cube faces
     cubeVerticesIndexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
-
-    // This array defines each face as two triangles, using the
-    // indices into the vertex array to specify each triangle's
-    // position.
-
     var cubeVertexIndices = [
         0,  1,  2,      0,  2,  3,    // front
         4,  5,  6,      4,  6,  7,    // back
@@ -151,12 +144,10 @@ function initCubeBuffer() {
         16, 17, 18,     16, 18, 19,   // right
         20, 21, 22,     20, 22, 23    // left
     ]
-
-    // Now send the element array to GL
-
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
                 new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
 
+    // cube colors
     var colors = [
         [1.0, 1.0, 1.0, 1.0], // front face: white
         [1.0, 0.0, 0.0, 1.0], // back face: red
@@ -175,23 +166,6 @@ function initCubeBuffer() {
     cubeVerticesColorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesColorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(generatedColors), gl.STATIC_DRAW);
-
-    /*
-    cubeOutlineIndexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeOutlineIndexBuffer);
-
-    var cubeOutlineIndices = [
-        0,  1,  2,  3,  0,
-        4,  5,  6,  7,  4,
-        8,  9,  10, 11, 8,
-        12, 13, 14, 15, 12,
-        16, 17, 18, 19, 16,
-        20, 21, 22, 23, 20
-    ]
-
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-        new Uint16Array(cubeOutlineIndices), gl.STATIC_DRAW);
-    */
 }
 
 function drawScene() {
@@ -204,6 +178,13 @@ function drawScene() {
 
 function drawCube() {
     initCubeBuffer();
+    // cube outline
+    gl.disableVertexAttribArray(vertexColor);
+    gl.lineWidth(5.0);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeOutlineBuffer);
+    gl.drawElements(gl.LINE_LOOP, 30, gl.UNSIGNED_SHORT, 0);
+    gl.enableVertexAttribArray(vertexColor);
+    // cube vertices
     gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesBuffer);
     gl.vertexAttribPointer(vertexPosition, 3, gl.FLOAT, false, 0, 0);
     // cube colors
@@ -212,13 +193,6 @@ function drawCube() {
     // cube faces
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
     gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
-
-    // cube outline
-    /*
-       gl.lineWidth(1.0);
-       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeOutlineIndexBuffer);
-       gl.drawElements(gl.LINE_LOOP, 30, gl.UNSIGNED_SHORT, 0);
-     */
 
 }
 
@@ -248,6 +222,8 @@ function start() {
         gl.clearColor(0.0, 0.0, 1.0, 1.0);
         gl.enable(gl.DEPTH_TEST);
         gl.depthFunc(gl.LEQUAL);
+        gl.enable(gl.CULL_FACE);
+        gl.cullFace(gl.BACK);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         tick();
     }
@@ -270,7 +246,7 @@ function rotate(event) {
         y_new = event.pageY;
         delta_x = (x_new - x_init) / 10;
         delta_y = (y_new - y_init) / 10;
-        var axis = [-delta_x, delta_y, 0];
+        var axis = [-delta_y, delta_x, 0];
         var degrees = Math.sqrt(delta_x * delta_x + delta_y * delta_y);
         var newRotationMatrix = mat4.create();
         mat4.rotate(rotationMatrix, rotationMatrix, degreesToRadians(degrees), axis);
