9b93f61df71dfcd118c828a7ed4c0a9b78955c80
Tiffany Wang <tinnywang@gmail.com>
2020-06-20 19:17:09 -0700

    add comment

--- js/rubiks.js
+++ js/rubiks.js
@@ -433,6 +433,8 @@ function rotate(event) {
 }
 
 function startRotate(event) {
+    // The Rubik's cube can be rotated with right mouse while it's being scrambled, but
+    // individual layers of the cube cannot be rotated with left mouse.
     if (!isScrambling && isLeftMouse(event)) {
         initIntersection = rubiksCube.select(event.pageX, event.pageY);
         if (initIntersection) {
